7a8a498daca8b30fccd0aa8bceff1bd214d39f6a
Said Amir <said.amir.kattan@gmail.com>
2020-12-14 19:50:28 +0200

    Add project files.

---
+++ Euler/Euler1.py
@@ -0,0 +1,16 @@
+num = 0
+
+total = 0
+
+while num != 333:
+    num += 1
+    multi = 3* num
+    if multi % 5 != 0:
+        total = total + (multi)
+num = 0
+while num != 199:
+    num += 1
+    total = total + (5* num)
+print(total)
+
+
